20aa29162b4236e817d5bd5ab32aa9de9793c9d9
dsr <bdbcat@yahoo.com>
2020-12-07 22:52:40 -0500

    Update to minimum compatible gl.h for wxQt (Android sysroot) build.

--- GL/src/gl.c
+++ GL/src/gl.c
@@ -1,4 +1,34 @@
+#include "../include/gl.h"
 
-char glBegin(){
-  return '0';
+void glBegin(GLenum mode){
 }
+
+void glFrustum(GLfloat left, GLfloat right, GLfloat bottom,
+               GLfloat top, GLfloat zNear, GLfloat zFar)
+{
+}
+
+void glTexCoord2f(GLfloat s, GLfloat t)
+{
+}
+
+void glVertex3f(GLfloat x, GLfloat y, GLfloat z)
+{
+}
+
+void glNormal3f(GLfloat nx, GLfloat ny, GLfloat nz)
+{
+}
+
+void glColor4f(GLfloat r, GLfloat g, GLfloat b, GLfloat a)
+{
+}
+
+void glColor3f(GLfloat r, GLfloat g, GLfloat b)
+{
+}
+
+void glEnd()
+{
+}
+
